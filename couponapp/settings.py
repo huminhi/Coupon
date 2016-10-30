6b0bac8dbcb1f33d032269c17a02b46a89a00a83
Hieu Huynh <huminhi@gmail.com>
2016-10-30 23:47:08 +0700

    update

--- couponapp/settings.py
+++ couponapp/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     # 'couponapp'
 ]
 
@@ -127,5 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    '/couponapp/static/',
+)
 
 # MEDIA_ROOT = 'media/'
